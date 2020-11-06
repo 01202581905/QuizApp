69cc958ef538af0a65c00026d6241dfa9ba357bc
AnhKhoa <trongphoenix@gmail.com>
2020-11-06 10:51:27 +0700

    add modal result, final

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ const quizsData = [
         b: 3,
         c: 4,
         d: 5,
-        correct: c
+        correct: "c"
     },
     {
         question: 'What is the most used programing languge in 2019 ?',
@@ -14,7 +14,7 @@ const quizsData = [
         b: 'C',
         c: 'Js',
         d: 'Python',
-        correct: c
+        correct: "c"
     },
     {
         question: 'What does HTML stand for',
@@ -22,7 +22,7 @@ const quizsData = [
         b: 'Language Programing',
         c: 'IDE',
         d: 'Framework Front-end',
-        correct: a
+        correct: "a"
     }
 ];
 const question = document.querySelector('#question');
@@ -31,16 +31,20 @@ const ansB = document.querySelector('.label_b');
 const ansC = document.querySelector('.label_c');
 const ansD = document.querySelector('.label_d');
 const submit = document.querySelector('#submit');
-const err = document.querySelector('#err');
-const listAns = document.querySelectorAll('.answer');
 const totalQuesElm = document.querySelector('#total');
 const currQuesElm = document.querySelector('#current');
+const listResult = document.querySelector('#listresult');
+const close = document.querySelector('#close');
+const err = document.querySelector('#err');
+const modal = document.querySelector('.quizcontainer__modal');
 let currentQues = 0;
 let answer = undefined;
+let arrAns = [];
 
 function quiz(){
-    if(currentQues == quizsData.length) 
-        currentQues = 0;
+    if(currentQues == quizsData.length) {
+        return true;
+    }
     const currentData = quizsData[currentQues];
     animationText(question, currentData.question, 400);
     animationText(ansA, currentData.a , 400);
@@ -48,6 +52,7 @@ function quiz(){
     animationText(ansC, currentData.c , 400);
     animationText(ansD, currentData.d , 400);
     currQuesElm.innerText = currentQues + 1;
+    return false;
 };
 
 function getSelect() {
@@ -55,7 +60,7 @@ function getSelect() {
     if(!rightAns) {
         return false;
     }
-    return true;
+    return rightAns;
 };
 
 function animationText(elem, value, duration) {
@@ -64,7 +69,6 @@ function animationText(elem, value, duration) {
         elem.innerText = value;
         elem.style.opacity = 1;
     }, duration);
-    //
 };
 
 submit.addEventListener('click', function(event){
@@ -74,14 +78,32 @@ submit.addEventListener('click', function(event){
         event.preventDefault();
     } else {
         err.classList.remove('showerr');
-        listAns.forEach(item => {
-            item.checked = false;
-        });
+        isAns.checked = false;
+        arrAns.push(isAns.id);
         currentQues++;
-        quiz();
+        const isFinal = quiz();
+        if(isFinal) {
+            for(let i = 0;i < quizsData.length;i++) {
+                if(quizsData[i].correct == arrAns[i]) {
+                    listResult.insertAdjacentHTML("beforeEnd", `<li><p>: ${quizsData[i].correct}<img src="https://lh3.googleusercontent.com/proxy/8-KpWdWv3m0GiAMExPHg9xbQVFkNXgmJX3icF3nnyhzU4A0v9c_4PD2Wz3V-FYQHY0muKglSrHktoeLi0YHjulX1xsZbZdo" alt="checked"></p></li>`);
+                } else {
+                    listResult.insertAdjacentHTML("beforeEnd", `
+                    <li><p>: ${arrAns[i]}<img src="https://icon-library.com/images/x-png-icon/x-png-icon-8.jpg" alt="falsechecked"> (${quizsData[i].correct})</p></li>`);
+                }
+            }
+            modal.classList.add('show');
+        }
     }
         
 });
+
+close.addEventListener('click', function(){
+    currentQues = 0;
+    modal.classList.remove('show');
+    arrAns = [];
+    quiz();
+});
+
 totalQuesElm.innerText = quizsData.length;
 quiz();
 
